6d51cdafe55a7f7a0724042a0c84f1e8ba63195d
Sabari-dharshini <vsabaridharshini@gmail.com>
2025-04-05 15:48:20 +0530

    Add files via upload

---
+++ TCS set 3/tcs3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class tcs3{
+  public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+         int n=8;
+         double fare;
+         int paths[]={800,600,750,900,1400,1200,1100,1500};
+         String []BusStops={"TH","GA","IC","HA","TE","LU","NI","CA"};
+         for(int i=0;i<paths.length;i++){
+               if(paths[i]==1000){
+                  fare=5;
+                 }else{
+                   fare=Math.ceil();
+               }
+}
+
+         
+}
+}
